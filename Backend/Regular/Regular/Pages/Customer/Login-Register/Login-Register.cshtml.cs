000f554605dd2de17601778c34ac355498c77858
CC-Mehdi <kiarashjahangir6@gmail.com>
2023-12-10 21:02:49 +0330

    Backend : Add login-register pages

---
+++ Backend/Regular/Regular/Pages/Customer/Login-Register/Login-Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Regular.Pages.Customer.Login_Register
+{
+    public class Login_RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
