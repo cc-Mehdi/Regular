e6a4dbe83f9350ef48b01539bbad916ef60cc55f
CC-Mehdi <kiarashjahangir6@gmail.com>
2023-12-10 18:34:25 +0330

    init backend project

---
+++ Backend/Regular/Datalayer/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<Coffee> Coffee { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<BuyHistory> BuyHistories { get; set; }
+    }
+}
