e4305351d37ed70258a6fb8b923cc9a70320e67b
CC-Mehdi <kiarashjahangir6@gmail.com>
2023-12-10 19:10:48 +0330

    Backend : add tasks model

---
+++ Backend/Regular/Datalayer/Models/Tasks.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Datalayer.Models
+{
+    public class Tasks
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "عنوان وظیفه")]
+        [Required(ErrorMessage = "لطفا {0} را صحیح وارد کنید")]
+        [MaxLength(200, ErrorMessage = "مقدار {0} بیش از حد مجاز طولانی است")]
+        [DataType(DataType.Text)]
+        public string Title { get; set; }
+
+        [ForeignKey("Projects")]
+        public int ProjectId { get; set; }
+        public Projects Project { get; set; }
+
+        [ForeignKey("Users")]
+        public int UserId { get; set; }
+        public Users User { get; set; }
+
+        [Display(Name = "نوع وظیفه")]
+        [Required(ErrorMessage = "لطفا {0} را صحیح وارد کنید")]
+        [MaxLength(100, ErrorMessage = "مقدار {0} بیش از حد مجاز طولانی است")]
+        [DataType(DataType.Text)]
+        public string TaskType { get; set; }
+
+        [Display(Name = "زمان تقریبی")]
+        [Required(ErrorMessage = "لطفا {0} را صحیح وارد کنید")]
+        [MaxLength(50, ErrorMessage = "مقدار {0} بیش از حد مجاز طولانی است")]
+        [DataType(DataType.Text)]
+        public string EstimateTime { get; set; }
+
+        [Display(Name = "زمان ثبت شده")]
+        [Required(ErrorMessage = "لطفا {0} را صحیح وارد کنید")]
+        [MaxLength(50, ErrorMessage = "مقدار {0} بیش از حد مجاز طولانی است")]
+        [DataType(DataType.Text)]
+        public string LogedTime { get; set; }
+
+        [Display(Name = "وضعیت وظیفه")]
+        [Required(ErrorMessage = "لطفا {0} را صحیح وارد کنید")]
+        [MaxLength(100, ErrorMessage = "مقدار {0} بیش از حد مجاز طولانی است")]
+        [DataType(DataType.Text)]
+        public string TaskStatus { get; set; }
+
+        [Display(Name = "سطح اولویت")]
+        [Required(ErrorMessage = "لطفا {0} را صحیح وارد کنید")]
+        [MaxLength(100, ErrorMessage = "مقدار {0} بیش از حد مجاز طولانی است")]
+        [DataType(DataType.Text)]
+        public string TaskPriority { get; set; }
+
+        [Display(Name = "مشکل در نسخه")]
+        [Required(ErrorMessage = "لطفا {0} را صحیح وارد کنید")]
+        [MaxLength(50, ErrorMessage = "مقدار {0} بیش از حد مجاز طولانی است")]
+        [DataType(DataType.Text)]
+        public string BeforeVersion { get; set; }
+
+        [Display(Name = "اصلاح در نسخه")]
+        [Required(ErrorMessage = "لطفا {0} را صحیح وارد کنید")]
+        [MaxLength(50, ErrorMessage = "مقدار {0} بیش از حد مجاز طولانی است")]
+        [DataType(DataType.Text)]
+        public string AfterVersion { get; set; }
+
+        [Display(Name = "توضیحات")]
+        [Required(ErrorMessage = "لطفا {0} را صحیح وارد کنید")]
+        [MaxLength(500, ErrorMessage = "مقدار {0} بیش از حد مجاز طولانی است")]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+    }
+}
