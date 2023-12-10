ae2fd1aed6f40a5ac5d69716fd45187a4559dfc3
CC-Mehdi <kiarashjahangir6@gmail.com>
2023-12-10 18:51:00 +0330

    Backend : and datalayer and create users model

---
+++ Backend/Regular/Datalayer/Models/Users.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Datalayer.Models
+{
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را صحیح وارد کنید")]
+        [MaxLength(300, ErrorMessage = "مقدار {0} بیش از حد مجاز طولانی است")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را صحیح وارد کنید")]
+        [MaxLength(300, ErrorMessage = "مقدار {0} بیش از حد مجاز طولانی است")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را صحیح وارد کنید")]
+        [MaxLength(200, ErrorMessage = "مقدار {0} بیش از حد مجاز طولانی است")]
+        [DataType(DataType.Text)]
+        public string Username { get; set; }
+
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = "لطفا {0} را صحیح وارد کنید")]
+        [MaxLength(150, ErrorMessage = "مقدار {0} بیش از حد مجاز طولانی است")]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "نام خانوادگی")]
+        [Required(ErrorMessage = "لطفا {0} را صحیح وارد کنید")]
+        [MaxLength(150, ErrorMessage = "مقدار {0} بیش از حد مجاز طولانی است")]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+    }
+}
