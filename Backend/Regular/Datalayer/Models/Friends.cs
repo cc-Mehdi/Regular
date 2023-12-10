08106dd5d148968384703096c09c591da846e7f6
CC-Mehdi <kiarashjahangir6@gmail.com>
2023-12-10 19:13:02 +0330

    Backend : add friends model

---
+++ Backend/Regular/Datalayer/Models/Friends.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Datalayer.Models
+{
+    public class Friends
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Users")]
+        public int UserId1 { get; set; }
+        public Users User1 { get; set; }
+
+        [ForeignKey("Users")]
+        public int UserId2 { get; set; }
+        public Users User2 { get; set; }
+    }
+}
