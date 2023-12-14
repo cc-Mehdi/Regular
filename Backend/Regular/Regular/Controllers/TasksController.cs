a500a83f0a8d4340ddefbd2a164bc3551e7c8c4b
CC-Mehdi <kiarashjahangir6@gmail.com>
2023-12-14 23:12:40 +0330

    Backend > Manage > Tasks : add tasks api controller

---
+++ Backend/Regular/Regular/Controllers/TasksController.cs
@@ -0,0 +1,34 @@
+﻿using DataLayer.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Regular.Controllers
+{
+    [Route("/api/{controller}")]
+    [ApiController]
+    public class TasksController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TasksController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var tasksList = _unitOfWork.TasksRepository.GetAll();
+            return Json(new { data = tasksList });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.TasksRepository.GetFirstOrDefault(u => u.Id == id);
+
+            _unitOfWork.TasksRepository.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "حذف با موفقیت انجام شد" });
+        }
+    }
+}
