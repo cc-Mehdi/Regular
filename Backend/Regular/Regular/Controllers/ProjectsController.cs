c8f2444524479d524a2b41a476c485c8add9e0d5
CC-Mehdi <kiarashjahangir6@gmail.com>
2023-12-15 23:28:02 +0330

    Backend > Manage > Projects : add project api

---
+++ Backend/Regular/Regular/Controllers/ProjectsController.cs
@@ -0,0 +1,34 @@
+﻿using DataLayer.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Regular.Controllers
+{
+    [Route("/api/{controller}")]
+    [ApiController]
+    public class ProjectsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProjectsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var projectsList = _unitOfWork.ProjectsRepository.GetAll();
+            return Json(new { data = projectsList });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.ProjectsRepository.GetFirstOrDefault(u => u.Id == id);
+
+            _unitOfWork.ProjectsRepository.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "حذف با موفقیت انجام شد" });
+        }
+    }
+}
