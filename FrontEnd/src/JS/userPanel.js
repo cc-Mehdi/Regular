78deeda1a1d889d943415044638473935c8963b2
cc-mehdi <kiarashjahangir6@gmail.com>
2024-05-07 19:54:43 +0330

    feat | User Panel : complete basics of user panel

---
+++ FrontEnd/src/JS/userPanel.js
@@ -0,0 +1,73 @@
+// Config Bootstrap ToolTip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+window.addEventListener("resize", function () {
+    if (this.window.innerWidth <= 1200)
+        HideoffcanvasMenu();
+    else
+        ShowoffcanvasMenu();
+});
+
+window.addEventListener("load", function () {
+    if (this.window.innerWidth <= 1200)
+        HideoffcanvasMenu();
+    else
+        ShowoffcanvasMenu();
+});
+
+function HideoffcanvasMenu() {
+    document.getElementById("btnShowMenuToggler").classList.remove("visually-hidden");
+    document.getElementById("offcanvasMenu").classList.remove("show");
+
+    document.getElementById("menuBox-Temp").classList.remove("col-xxl-4");
+    document.getElementById("mainBox").classList.remove("col-xxl-6");
+    document.getElementById("mainBox").classList.add("col-xxl-11");
+
+    document.getElementById("menuBox-Temp").classList.remove("col-lg-4");
+    document.getElementById("mainBox").classList.remove("col-lg-11");
+    document.getElementById("mainBox").classList.add("col-lg-11");
+}
+
+function ShowoffcanvasMenu() {
+    document.getElementById("btnShowMenuToggler").classList.add("visually-hidden");
+    document.getElementById("offcanvasMenu").classList.add("show");
+
+    document.getElementById("menuBox-Temp").classList.add("col-xxl-4");
+    document.getElementById("mainBox").classList.add("col-xxl-6");
+    document.getElementById("mainBox").classList.remove("col-xxl-11");
+
+    document.getElementById("menuBox-Temp").classList.add("col-lg-4");
+    document.getElementById("mainBox").classList.add("col-lg-7");
+    document.getElementById("mainBox").classList.remove("col-lg-11");
+}
+
+function ToggleSecondOffcanvasMenu() {
+    document.getElementById("SecondOffcanvasMenu").classList.toggle("show");
+}
+
+// Function to handle click events on list items
+function handleListItemClick(event) {
+    // Get all list items with the 'userPanel-menuItem' class
+    const listItems = document.querySelectorAll('.userPanel-menuItem');
+  
+    // Remove the 'userPanel-menuItem-active' class from all list items
+    listItems.forEach(item => {
+      item.classList.remove('userPanel-menuItem-active');
+    });
+  
+    // Add the 'userPanel-menuItem-active' class to the clicked list item
+    event.currentTarget.classList.add('userPanel-menuItem-active');
+  }
+  
+  // Attach the click event listener to all list items with the 'userPanel-menuItem' class
+  function attachClickListeners() {
+    const listItems = document.querySelectorAll('.userPanel-menuItem');
+  
+    listItems.forEach(item => {
+      item.addEventListener('click', handleListItemClick);
+    });
+  }
+  
+  // Call the function to attach click listeners when the page is loaded
+  document.addEventListener('DOMContentLoaded', attachClickListeners);
